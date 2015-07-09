1374adc5f1c4f7ffd29583cbcb45d80a62328b3e
mandar_shinde <01.mandar@gmail.com>
2015-07-09 15:49:29 +0530

    added reboot and shutdown script

---
+++ Reboot.bat
@@ -0,0 +1,5 @@
+::
+:: Reboot Scripts
+::
+
+%systemroot%\System32\shutdown /r /f /t 00
---
+++ Shutdown.bat
@@ -0,0 +1,9 @@
+::
+:: Shutdown Scripts
+::
+
+@echo off
+CLS 
+
+
+%systemroot%\System32\shutdown /s /f /t 00
